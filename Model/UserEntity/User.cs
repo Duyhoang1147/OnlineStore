8b926d712c35a260ef9a9b6fefe9ecba5005ed66
Duyhoang1147 <115346797+Duyhoang1147@users.noreply.github.com>
2025-06-04 15:47:54 +0700

    add package Identity

---
+++ Model/UserEntity/User.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Model.UserEntity
+{
+    public class User
+    {
+        
+    }
+}
