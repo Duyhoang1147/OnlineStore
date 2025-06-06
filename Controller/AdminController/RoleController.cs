43fa686e834cfede0d97c6d91b2237dca2f558ae
Duyhoang1147 <115346797+Duyhoang1147@users.noreply.github.com>
2025-06-06 11:03:45 +0700

    Complete RoleController

--- Controller/AdminController/RoleController.cs
+++ Controller/AdminController/RoleController.cs
@@ -1,6 +1,8 @@
+using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using OnlineStore.Model.Dto.UserDto;
 
 namespace OnlineStore.Controller.AdminController
 {
@@ -27,6 +29,116 @@ namespace OnlineStore.Controller.AdminController
             .ToList();
 
             return Task.FromResult<IActionResult>(Ok(roles));
+        }
+
+        public async Task<IActionResult> Create([FromBody] RoleDto model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var existRole = await _roleManager.RoleExistsAsync(model.RoleName);
+            if (existRole)
+            {
+                return BadRequest("role has exist");
+            }
+
+            var role = await _roleManager.CreateAsync(new IdentityRole(model.RoleName));
+            if (role.Succeeded)
+            {
+                return Ok(role);
+            }
+            return BadRequest("Create fail");
+        }
+
+        public async Task<IActionResult> Update([FromBody] RoleDto model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var role = await _roleManager.FindByNameAsync(model.RoleName);
+            if (role == null)
+            {
+                return BadRequest("role is not exist");
+            }
+
+            role.Name = model.RoleName;
+            role.NormalizedName = model.RoleName.ToUpper();
+
+            var updateRole = await _roleManager.UpdateAsync(role);
+            if (updateRole.Succeeded)
+            {
+                return Ok("update successful");
+            }
+            return BadRequest("Update fail");
+        }
+
+        public async Task<IActionResult> Delete([FromBody] RoleDto model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var role = await _roleManager.FindByNameAsync(model.RoleName);
+            if (role == null)
+            {
+                return BadRequest("role is not exist");
+            }
+
+            var roleResult = await _roleManager.DeleteAsync(role);
+            if (roleResult.Succeeded)
+            {
+                return Ok("Delete successful");
+            }
+            return BadRequest("Delete fail");
+        }
+
+        public async Task<IActionResult> AddClaim([FromBody] RoleClaimDto model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var role = await _roleManager.FindByNameAsync(model.RoleName);
+            if (role == null)
+            {
+                return BadRequest("role is not exist");
+            }
+
+            var claim = await _roleManager.AddClaimAsync(role, new Claim(model.ClaimType, model.ClaimValue));
+            if (claim.Succeeded)
+            {
+                return Ok("Add claim successful");
+            }
+
+            return BadRequest("Add Claims fail");
+        }
+
+        public async Task<IActionResult> DeleteClaim([FromBody] RoleClaimDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var role = await _roleManager.FindByNameAsync(model.RoleName);
+            if (role == null)
+            {
+                return BadRequest("role not found");
+            }
+
+            var claim = await _roleManager.RemoveClaimAsync(role, new Claim(model.ClaimType, model.ClaimValue));
+            if (claim.Succeeded)
+            {
+                return Ok("Remove Claim successful");
+            }
+            
+            return BadRequest("Remove fails");
         }
     }
 }
--- Model/Dto/UserDto/RoleClaimDto.cs
+++ Model/Dto/UserDto/RoleClaimDto.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-using OnlineStore.Model.UserEntity;
+using Microsoft.Identity.Client;
 
 namespace OnlineStore.Model.Dto.UserDto
 {
-    public class RoleDto
+    public class RoleClaimDto
     {
         [Required]
         public required string RoleName { get; set; }
+        
+        [Required]
+        public required string ClaimType { get; set; }
+
+        [Required]
+        public required string ClaimValue { get; set; }
     }
 }
